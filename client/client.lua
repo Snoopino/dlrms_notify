2b8e1c4752eb85a892ed9c2ad0a0af0d9d5957c7
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-06-04 04:21:11 +0300

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,8 @@
+RegisterNetEvent("dlrms_notify")
+AddEventHandler("dlrms_notify", function(type, msg, duration)
+	SendNUIMessage({ 
+		notification = msg,
+		notification_type = type,
+		duration = duration,
+	})
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'gta5' }
+author 'Delarmuss'
+client_scripts {
+	'client/client.lua'
+}
+
+ui_page "html/ui.html"
+
+files {
+	"html/ui.html",
+	"html/js/panel.js",
+}   
