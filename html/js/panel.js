3eaab6dccadd1c784b139a6e6745a4d066461daa
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-06-27 16:52:07 +0300

    Update panel.js

--- html/js/panel.js
+++ html/js/panel.js
@@ -1,5 +1,6 @@
 window.addEventListener('message', function (event) {
 	var item = event.data;
+	
 	if (item.notification) {
 		if (item.notification_type == "success") {
 			dlrms.success(item.notification, {
@@ -46,111 +47,6 @@ window.addEventListener('message', function (event) {
 		info: '<svg viewBox="0 0 23.625 23.625"><path d="M11.812 0C5.289 0 0 5.289 0 11.812s5.289 11.813 11.812 11.813 11.813-5.29 11.813-11.813S18.335 0 11.812 0zm2.459 18.307c-.608.24-1.092.422-1.455.548a3.838 3.838 0 0 1-1.262.189c-.736 0-1.309-.18-1.717-.539s-.611-.814-.611-1.367c0-.215.015-.435.045-.659a8.23 8.23 0 0 1 .147-.759l.761-2.688c.067-.258.125-.503.171-.731.046-.23.068-.441.068-.633 0-.342-.071-.582-.212-.717-.143-.135-.412-.201-.813-.201-.196 0-.398.029-.605.09-.205.063-.383.12-.529.176l.201-.828c.498-.203.975-.377 1.43-.521a4.225 4.225 0 0 1 1.29-.218c.731 0 1.295.178 1.692.53.395.353.594.812.594 1.376 0 .117-.014.323-.041.617a4.129 4.129 0 0 1-.152.811l-.757 2.68a7.582 7.582 0 0 0-.167.736 3.892 3.892 0 0 0-.073.626c0 .356.079.599.239.728.158.129.435.194.827.194.185 0 .392-.033.626-.097.232-.064.4-.121.506-.17l-.203.827zm-.134-10.878a1.807 1.807 0 0 1-1.275.492c-.496 0-.924-.164-1.28-.492a1.57 1.57 0 0 1-.533-1.193c0-.465.18-.865.533-1.196a1.812 1.812 0 0 1 1.28-.497c.497 0 .923.165 1.275.497.353.331.53.731.53 1.196 0 .467-.177.865-.53 1.193z"/></svg>',
 		error: '<svg viewBox="0 0 51.976 51.976"><path d="M44.373 7.603c-10.137-10.137-26.632-10.138-36.77 0-10.138 10.138-10.137 26.632 0 36.77s26.632 10.138 36.77 0c10.137-10.138 10.137-26.633 0-36.77zm-8.132 28.638a2 2 0 0 1-2.828 0l-7.425-7.425-7.778 7.778a2 2 0 1 1-2.828-2.828l7.778-7.778-7.425-7.425a2 2 0 1 1 2.828-2.828l7.425 7.425 7.071-7.071a2 2 0 1 1 2.828 2.828l-7.071 7.071 7.425 7.425a2 2 0 0 1 0 2.828z"/></svg>'
 	}
-
-	const styles = `
-		:root{
-			--successColor: #2CA342;
-			--errorColor: #D60A2E;
-			--informationColor: #1271EC;
-			--warningColor: #FCC631;
-		}
-
-		.dlrms-container {
-			position: fixed;
-			width: 100%;
-			height: 100vh;
-			top: 0;
-			left: 0;
-			z-index: 9999;
-			display: flex;
-			flex-direction: column;
-			pointer-events: none;
-			-webkit-font-smoothing: antialiased;
-    		-moz-osx-font-smoothing: grayscale;
-		}
-
-		.dlrms-row {
-			display: flex;
-			justify-content: space-between;
-		}
-
-		.dlrms-col {
-			flex: 1;
-			margin: 10px;
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-		}
-
-		.dlrms-col.top-left,
-		.dlrms-col.bottom-left {
-			align-items: flex-start;
-		}
-
-		.dlrms-col.top-right,
-		.dlrms-col.bottom-right {
-			align-items: flex-end;
-		}
-
-		.dlrms-card {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			padding: 10px 10px 10px 39px;
-			border-radius: 4px;
-			transition: .4s all ease-in-out;
-			position: relative;
-			background: #fff;
-			color: white;
-		}
-
-		svg {
-			position: absolute;
-			left: 10px;
-			width: 19px;
-			height: 19px;
-			fill: #fff;
-		}
-
-		.dlrms-card.success {
-			box-shadow: 0 0 2px var(--successColor);
-			background: var(--successColor);
-		}
-		
-		.dlrms-card.warn {
-			box-shadow: 0 0 2px var(--warningColor);
-			background: var(--warningColor);
-		}
-
-		.dlrms-card.info {
-			box-shadow: 0 0 2px var(--informationColor);
-			background: var(--informationColor);
-		}
-
-		.dlrms-card.error {
-			box-shadow: 0 0 2px var(--errorColor);
-			background: var(--errorColor);
-		}
-
-		.dlrms-card.text-group {
-			/* margin-left: 10px; */
-		}
-
-		.dlrms-card h4 {
-			margin-bottom: 10px;
-			font-size: 16px;
-			font-weight: 500;
-		}
-
-		.dlrms-card p {
-			margin: 0;
-			font-size: 14px;
-		}
-	`
-
-	const styleSheet = document.createElement("style")
-	styleSheet.innerText = styles.replace((/  |\r\n|\n|\r/gm), "")
-	document.head.appendChild(styleSheet)
 
 	const dlrmsContainer = document.createElement("div")
 	dlrmsContainer.className = "dlrms-container"
