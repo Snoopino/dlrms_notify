2e7f1fbca866b9a91c7cb04293f4cd7e1e7ef47e
Delarmuss <83409265+Delarmuss@users.noreply.github.com>
2021-07-21 21:49:04 +0300

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,11 +1,6 @@
 RegisterNetEvent("dlrms_notify")
 AddEventHandler("dlrms_notify", function(type, msg, duration)
-	SendNUIMessage({ 
-		notification = msg,
-		notification_type = type,
-		duration = duration,
-		transactionType = 'playSound',
-	})
+	SendAlert(type, msg, duration)
 end)
 
 function SendAlert(type, msg, duration)
